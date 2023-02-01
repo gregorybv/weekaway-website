292b695d1645e0dc3bb815b5e206e12e61e13bd0
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-02-01 11:05:50 +0300

    hero block add to project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import TopBar from "./components/TopBar";
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 function App() {
   return (
     <div className="App">
       <TopBar/>
       <Navbar/>
+      <Hero/>
     </div>
   );
 }
