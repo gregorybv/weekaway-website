6528a5aa853f69050f8cbef7d8723e4b7c48cfea
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-02-01 10:36:33 +0300

    nawbar block add icons to project

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {FaBars} from "react-icons/fa";
+import {BsChatSquareDots} from "react-icons/bs";
+import {FaFacebookF, FaTwitter, FaGooglePlusG, FaInstagram } from "react-icons/fa";
+
+const Navbar = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Navbar
+
+
+
