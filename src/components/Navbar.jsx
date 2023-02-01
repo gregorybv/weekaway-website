020f3571da83c2b954ea0c44a0f8ae2d3233c8c4
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-02-01 10:50:46 +0300

    nawbar block add style to project

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,49 @@
 import React from 'react';
-import {FaBars} from "react-icons/fa";
-import {BsChatSquareDots} from "react-icons/bs";
-import {FaFacebookF, FaTwitter, FaGooglePlusG, FaInstagram } from "react-icons/fa";
+// import {BsChatSquareDots} from "react-icons/bs";
+import {FaFacebookF, FaTwitter, FaGooglePlusG, FaInstagram, FaBars } from "react-icons/fa";
 
 const Navbar = () => {
   return (
-    <div>
-
+    <div className='w-full min-h-[50px] flex justify-between items-center absolute z-10 text-white bg-gray-700'>
+      <ul className='hidden sm:flex px-4'>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="#gallery">Gallery</a>
+        </li>
+        <li>
+          <a href="#deals">Deals</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+      <div>
+        <FaFacebookF className='mx-4'/>
+        <FaTwitter className='mx-4'/>
+        <FaGooglePlusG className='mx-4'/>
+        <FaInstagram className='mx-4'/>
+      </div>
+      <div>
+        <FaBars/>
+      </div>
+      <div>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="#gallery">Gallery</a>
+          </li>
+          <li>
+            <a href="#deals">Deals</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
