f01722d87dd541a281bf708c1dbdb65a19b605fc
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-02-01 12:29:07 +0300

    new images add to project

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
+    <>
       <TopBar/>
       <Navbar/>
       <Hero/>
@@ -19,7 +19,7 @@ function App() {
       <Gallery/>
       <Contact/>
       <Footer/>
-    </div>
+    </>
   );
 }
 
--- src/components/Activities.jsx
+++ src/components/Activities.jsx
@@ -4,19 +4,25 @@ const Activities = () => {
   return (
     <div className='max-w-[1140px] m-auto w-full md:flex mt-[-75px]'>
       <div className='relative p-4'>
-        <h3 className='absolute z-10 top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-white text-2xl font-bold'>Resorts</h3>
+        <h3
+          className='absolute z-10 top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-white text-2xl font-bold'>Resorts</h3>
         <img className='w-full h-full object-cover relative border-4 border-white shadow-lg'
-          src="https://images.unsplash.com/photo-1613331134165-1445f441513c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="/"/>
+             src="https://images.unsplash.com/photo-1613331134165-1445f441513c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+             alt="/"/>
       </div>
       <div className='relative p-4'>
-        <h3 className='absolute z-10 top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-white text-2xl font-bold'>Cruises</h3>
+        <h3
+          className='absolute z-10 top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-white text-2xl font-bold'>Cruises</h3>
         <img className='w-full h-full object-cover relative border-4 border-white shadow-lg'
-          src="https://images.unsplash.com/photo-1471922694854-ff1b63b20054?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=872&q=80" alt="/"/>
+             src="https://images.unsplash.com/photo-1554254648-2d58a1bc3fd5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80"
+             alt="/"/>
       </div>
       <div className='relative p-4'>
-        <h3 className='absolute z-10 top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-white text-2xl font-bold'>Excursions</h3>
+        <h3
+          className='absolute z-10 top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-white text-2xl font-bold'>Excursions</h3>
         <img className='w-full h-full object-cover relative border-4 border-white shadow-lg'
-          src="https://images.unsplash.com/photo-1474433188271-d3f339f41911?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="/"/>
+             src="https://images.unsplash.com/photo-1558946038-13da6dbf3909?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80"
+             alt="/"/>
       </div>
     </div>
   )
