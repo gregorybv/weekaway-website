d4add0a8e04eb86f1121a529c1bb478ae1b4f02b
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-02-01 11:33:12 +0300

    activities block add images to project

--- src/components/Activities.jsx
+++ src/components/Activities.jsx
@@ -3,7 +3,18 @@ import React from 'react';
 const Activities = () => {
   return (
     <div>
-
+      <div>
+        <h3>Resorts</h3>
+        <img src="https://images.unsplash.com/photo-1613331134165-1445f441513c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="/"/>
+      </div>
+      <div>
+        <h3>Resorts</h3>
+        <img src="https://images.unsplash.com/photo-1471922694854-ff1b63b20054?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=872&q=80" alt="/"/>
+      </div>
+      <div>
+        <h3>Resorts</h3>
+        <img src="https://images.unsplash.com/photo-1474433188271-d3f339f41911?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="/"/>
+      </div>
     </div>
   )
 }
